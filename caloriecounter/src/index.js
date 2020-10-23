774eaca0802f610af22acf8168e757840a538f28
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-23 17:05:33 +0100

    structured our app and added views

--- caloriecounter/src/index.js
+++ caloriecounter/src/index.js
@@ -1,1 +1,8 @@
-console.log('It Worked!');
+import initModel from "./Model";
+import view from "./View";
+import update from "./Update";
+import app from "./App";
+
+const node = document.getElementById('app');
+
+app(initModel, view, update, node);
