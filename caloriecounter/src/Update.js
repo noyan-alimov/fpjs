da3c038c7d31fff8cf2015b7a293ffa1f3a30886
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-24 14:21:43 +0100

    implemented show form, accept user input and save new meal features

--- caloriecounter/src/Update.js
+++ caloriecounter/src/Update.js
@@ -1,5 +1,69 @@
+import * as R from 'ramda';
+
+const MSGS = {
+    SHOW_FORM: 'SHOW_FORM',
+    MEAL_INPUT: 'MEAL_INPUT',
+    CALORIES_INPUT: 'CALORIES_INPUT',
+    SAVE_MEAL: 'SAVE_MEAL'
+};
+
+export function showFormMsg(showForm) {
+    return {
+        type: MSGS.SHOW_FORM,
+        showForm
+    }
+}
+
+export function mealInputMsg(description) {
+    return {
+        type: MSGS.MEAL_INPUT,
+        description
+    }
+}
+
+export function caloriesInputMsg(calories) {
+    return {
+        type: MSGS.CALORIES_INPUT,
+        calories
+    }
+}
+
+export function saveMealMsg() {
+    return {
+        type: MSGS.SAVE_MEAL
+    }
+}
+
 function update(msg, model) {
-    return model;
+    switch (msg.type) {
+        case MSGS.SHOW_FORM:
+            return { ...model, showForm: msg.showForm, description: '', calories: 0 }
+        case MSGS.MEAL_INPUT:
+            return { ...model, description: msg.description }
+        case MSGS.CALORIES_INPUT:
+            const calories = R.pipe(
+                parseInt,
+                R.defaultTo(0)
+            )(msg.calories)
+            return { ...model, calories }
+        case MSGS.SAVE_MEAL:
+            return add(msg, model)
+        default:
+            return model;
+    }
+}
+
+function add(msg, model) {
+    const { nextId, description, calories, meals } = model
+    const meal = { id: nextId, description, calories }
+    return {
+        ...model,
+        meals: [...meals, meal],
+        nextId: nextId + 1,
+        description: '',
+        calories: 0,
+        showForm: false
+    }
 }
 
 export default update;
--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,16 +1,18 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { showFormMsg, mealInputMsg, caloriesInputMsg, saveMealMsg } from './Update'
 
 const { pre, div, h1, button, form, input, label } = hh(h);
 
-function fieldSet(labelText, inputValue) {
+function fieldSet(labelText, inputValue, oninput) {
     return div([
         label({ className: 'db mb1' }, labelText),
         input(
             { 
                 className: 'pa2 input-reset ba w-100 mb2',
                 type: 'text',
-                value: inputValue
+                value: inputValue,
+                oninput
             }
         )
     ])
@@ -28,7 +30,8 @@ function buttonSet(dispatch) {
         button(
             {
                 className: 'f3 pv2 ph3 bn bg-light-gray dim',
-                type: 'button'
+                type: 'button',
+                onclick: () => dispatch(showFormMsg(false))
             },
             'Cancel'
         )
@@ -40,16 +43,34 @@ function formView(dispatch, model) {
     if (showForm) {
         return form(
             {
-                className: 'w100 mv2'
+                className: 'w100 mv2',
+                onsubmit: e => {
+                    e.preventDefault()
+                    dispatch(saveMealMsg())
+                }
             },
             [
-                fieldSet('Meals', description),
-                fieldSet('Calories', calories || ''),
+                fieldSet(
+                    'Meals', 
+                    description,
+                    e => dispatch(mealInputMsg(e.target.value))
+                ),
+                fieldSet(
+                    'Calories', 
+                    calories || '',
+                    e => dispatch(caloriesInputMsg(e.target.value))
+                ),
                 buttonSet(dispatch)
             ]
         )
     }
-    return button({ className: 'f3 pv2 ph3 bg-blue white bn' }, 'Add Meal')
+    return button(
+        { 
+            className: 'f3 pv2 ph3 bg-blue white bn',
+            onclick: () => dispatch(showFormMsg(true))
+        }, 
+        'Add Meal'
+    )
 }
 
 function view(dispatch, model) {
