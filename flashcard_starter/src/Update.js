da1563c569b44a2bf88d9c0bf533866602653af3
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-24 23:55:13 +0100

    crud functionality for flashcards finished

--- flashcard_starter/src/Update.js
+++ flashcard_starter/src/Update.js
@@ -1,7 +1,140 @@
 import * as R from 'ramda';
 
-function update(msg, model) {
-  return model;
+const MSGS = {
+  SHOW_FORM: 'SHOW_FORM',
+  QUESTION_INPUT: 'QUESTION_INPUT',
+  ANSWER_INPUT: 'ANSWER_INPUT',
+  SAVE_FLASHCARD: 'SAVE_FLASHCARD',
+  DELETE_FLASHCARD: 'DELETE_FLASHCARD',
+  EDIT_FLASHCARD: 'EDIT_FLASHCARD',
+  SHOW_ANSWER_OF_FLASHCARD: 'SHOW_ANSWER_OF_FLASHCARD'
 }
+
+export const showFormMsg = showForm => ({
+  type: MSGS.SHOW_FORM,
+  showForm
+});
+
+export const questionInputMsg = question => ({
+  type: MSGS.QUESTION_INPUT,
+  question
+});
+
+export const answerInputMsg = answer => ({
+  type: MSGS.ANSWER_INPUT,
+  answer
+});
+
+export const saveFlashcardMsg = { type: MSGS.SAVE_FLASHCARD };
+
+export const deleteFlashcardMsg = flashcardId => ({
+  type: MSGS.DELETE_FLASHCARD,
+  flashcardId
+});
+
+export const editFlashcardMsg = flashcardId => ({
+  type: MSGS.EDIT_FLASHCARD,
+  flashcardId
+});
+
+export const showAnswerOfFlashcardMsg = flashcardId => ({
+  type: MSGS.SHOW_ANSWER_OF_FLASHCARD,
+  flashcardId
+});
+
+function update(msg, model) {
+  switch (msg.type) {
+    case MSGS.SHOW_FORM:
+      const { showForm } = msg;
+      return { ...model, showForm }
+    case MSGS.QUESTION_INPUT:
+      const { question } = msg;
+      return { ...model, question }
+    case MSGS.ANSWER_INPUT:
+      const { answer } = msg;
+      return { ...model, answer }
+    case MSGS.SAVE_FLASHCARD:
+      if (model.editId !== null) {
+        return editFlashcard(model)
+      }
+      return addFlashcard(model)
+    case MSGS.DELETE_FLASHCARD:
+      const flashcards = R.filter(
+        flashcard => flashcard.id !== msg.flashcardId, 
+        model.flashcards
+      )
+      return { ...model, flashcards }
+    case MSGS.EDIT_FLASHCARD:
+      const flashcard = R.find(
+        flashcard => flashcard.id === msg.flashcardId,
+        model.flashcards
+      )
+      return {
+        ...model,
+        editId: msg.flashcardId,
+        question: flashcard.question,
+        answer: flashcard.answer,
+        showForm: true
+      }
+    case MSGS.SHOW_ANSWER_OF_FLASHCARD:
+      return showAnswerOfFlashcard(msg, model)
+    default:
+      return model;
+  }
+}
+
+const addFlashcard = model => {
+  const { question, answer, nextId, flashcards } = model;
+  const flashcard = { id: nextId, question, answer, showAnswer: false };
+  return {
+    ...model,
+    flashcards: [...flashcards, flashcard],
+    nextId: nextId + 1,
+    question: '',
+    answer: '',
+    showForm: false
+  };
+};
+
+const editFlashcard = model => {
+  const { question, answer, editId } = model;
+  const flashcards = R.map(
+    flashcard => {
+      if (flashcard.id === editId) {
+        return { ...flashcard, question, answer, showAnswer: false }
+      }
+      return flashcard
+    },
+    model.flashcards
+  )
+
+  return {
+    ...model,
+    flashcards,
+    question: '',
+    answer: '',
+    showForm: false
+  };
+};
+
+const showAnswerOfFlashcard = (msg, model) => {
+  const flashcards = R.map(
+    flashcard => {
+      if (flashcard.id === msg.flashcardId) {
+        return { ...flashcard, showAnswer: true }
+      }
+      return flashcard
+    },
+    model.flashcards
+  )
+
+  return {
+    ...model,
+    flashcards,
+    question: '',
+    answer: '',
+    showForm: false
+  };
+};
 
 export default update;
--- flashcard_starter/src/View.js
+++ flashcard_starter/src/View.js
@@ -1,29 +1,124 @@
 import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { 
+  showFormMsg, 
+  questionInputMsg, 
+  answerInputMsg, 
+  saveFlashcardMsg,
+  deleteFlashcardMsg,
+  editFlashcardMsg,
+  showAnswerOfFlashcardMsg
+} from "./Update";
 
 const { pre, div, h1, textarea, button, i } = hh(h);
 
-function inputTextArea(label) {
+const inputTextArea = (label, value, oninput) => {
   return div([
     div({ className: 'b f6 mv1' }, label),
-    textarea({ className: 'w-100 bg-washed-yellow outline-0' })
+    textarea(
+      { 
+        className: 'w-100 bg-washed-yellow outline-0',
+        value,
+        oninput
+      }
+    )
   ])
 }
 
-function formView(dispatch, model) {
-  return div({ className: 'w-third bg-light-yellow pa3' }, [
-    inputTextArea('Question'),
-    inputTextArea('Answer'),
-    button({ className: 'f4 ph3 pv2 br1 bg-gray bn white mv2' }, 'Save')
-  ])
+const formView = (dispatch, model) => {
+  if (model.showForm) {
+    return div({ className: 'w-third bg-light-yellow pa3' }, [
+      inputTextArea(
+        'Question', 
+        model.question,
+        e => dispatch(questionInputMsg(e.target.value))
+      ),
+      inputTextArea(
+        'Answer', 
+        model.answer,
+        e => dispatch(answerInputMsg(e.target.value))
+      ),
+      button(
+        { 
+          className: 'f4 ph3 pv2 br1 bg-gray bn white mv2',
+          onclick: () => dispatch(saveFlashcardMsg)
+        }, 'Save'
+      ),
+      button(
+        { 
+          className: 'f4 ph3 pv2 br1 bg-light-red bn white mv2 mh3',
+          onclick: () => dispatch(showFormMsg(false))
+        }, 'Cancel'
+      )
+    ])
+  }
 }
 
-function view(dispatch, model) {
+const answerView = flashcard => {
+  if (flashcard.showAnswer) {
+    return div(flashcard.answer)
+  }
+}
+
+const card = (dispatch, flashcard) => {
+  return div({ className: 'w-third bg-light-yellow pa3 ma2' }, [
+    div('Question'),
+    div(
+      {
+        className: 'pointer',
+        onclick: () => dispatch(editFlashcardMsg(flashcard.id)) 
+      }, 
+      flashcard.question
+    ),
+    div(
+      {
+        className: 'pointer',
+        onclick: () => dispatch(showAnswerOfFlashcardMsg(flashcard.id))
+      }, 
+      'Show Answer'
+    ),
+    answerView(flashcard),
+    button(
+      {
+        className: 'ma3 pa2 bg-light-red white br1 bn',
+        onclick: () => dispatch(deleteFlashcardMsg(flashcard.id)) 
+      }, 
+      'Delete'
+    )
+  ])
+};
+
+const flashcardsBody = (dispatch, flashcards) => {
+  const cards = R.map(
+    R.partial(card, [dispatch]),
+    flashcards
+  )
+
+  return div({ className: 'flex' }, cards)
+};
+
+const flashcardsView = (dispatch, flashcards) => {
+  if (flashcards.length === 0) {
+    return div({ className: 'mv2 i black-50' }, 'No flashcards to display...')
+  }
+  return div({ className: 'pa2' }, [
+    flashcardsBody(dispatch, flashcards)
+  ])
+};
+
+const view = (dispatch, model) => {
   return div({ className: 'mw8 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Flashcard Study'),
-    button({ className: 'pa2 br1 mv2 bg-green bn white' }, 'Add Flashcard'),
+    button(
+      { 
+        className: 'pa2 br1 mv2 bg-green bn white',
+        onclick: () => dispatch(showFormMsg(true))
+      }, 
+      'Add Flashcard'
+    ),
     formView(dispatch, model),
+    flashcardsView(dispatch, model.flashcards),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
