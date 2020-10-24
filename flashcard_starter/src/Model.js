54f8e4bb2e598dfbdac75d57a81914b49f998f7d
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-24 22:02:25 +0100

    defined model and implemented some views

--- flashcard_starter/src/Model.js
+++ flashcard_starter/src/Model.js
@@ -1,3 +1,10 @@
-const initModel = {};
+const initModel = {
+    question: '',
+    answer: '',
+    showForm: false,
+    nextId: 0,
+    editId: null,
+    flashcards: []
+};
 
 export default initModel;
--- flashcard_starter/src/View.js
+++ flashcard_starter/src/View.js
@@ -2,11 +2,28 @@ import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { div, h1 } = hh(h);
+const { pre, div, h1, textarea, button, i } = hh(h);
+
+function inputTextArea(label) {
+  return div([
+    div({ className: 'b f6 mv1' }, label),
+    textarea({ className: 'w-100 bg-washed-yellow outline-0' })
+  ])
+}
+
+function formView(dispatch, model) {
+  return div({ className: 'w-third bg-light-yellow pa3' }, [
+    inputTextArea('Question'),
+    inputTextArea('Answer'),
+    button({ className: 'f4 ph3 pv2 br1 bg-gray bn white mv2' }, 'Save')
+  ])
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw8 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Flashcard Study'),
+    button({ className: 'pa2 br1 mv2 bg-green bn white' }, 'Add Flashcard'),
+    formView(dispatch, model),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
