9f4e81d2bcf80c517d1591151b2f672c8649654c
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-24 18:50:58 +0100

    implemented temperature conversion functionality

--- tempunitconvert_starter/src/Update.js
+++ tempunitconvert_starter/src/Update.js
@@ -7,6 +7,64 @@ const MSGS = {
   RIGHT_UNIT: 'RIGHT_UNIT'
 }
 
+function FtoC(temp) {
+  return 5 / 9 * (temp - 32)
+}
+
+function CtoF(temp) {
+  return 9 / 5 * temp + 32
+}
+
+function KtoC(temp) {
+  return temp - 237.15
+}
+
+function CtoK(temp) {
+  return temp + 237.15
+}
+
+const FtoK = R.pipe(FtoC, CtoK)
+const KtoF = R.pipe(KtoC, CtoF)
+
+const unitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF
+  }
+}
+
+function convertFromToTemp(fromUnit, fromTemp, toUnit) {
+  const convertFn = R.pathOr(
+    R.identity,
+    [fromUnit, toUnit],
+    unitConversions
+  );
+  return convertFn(fromTemp)
+}
+
+function convert(model) {
+  const { leftValue, leftUnit, rightValue, rightUnit } = model
+
+  const [fromUnit, fromTemp, toUnit] = 
+    model.sourceLeft ?
+    [leftUnit, leftValue, rightUnit] :
+    [rightUnit, rightValue, leftUnit]
+
+  const otherValue = convertFromToTemp(fromUnit, fromTemp, toUnit)
+
+  return model.sourceLeft ?
+    { ...model, rightValue: otherValue } :
+    { ...model, leftValue: otherValue }
+}
+
 export function leftInputMsg(leftValue) {
   return {
     type: MSGS.LEFT_INPUT,
@@ -44,19 +102,19 @@ function update (msg, model) {
         return { ...model, sourceLeft: true, leftValue: '', rightValue: '' }
       }
       const leftValue = toInt(msg.leftValue)
-      return { ...model, sourceLeft: true, leftValue }
+      return convert({ ...model, sourceLeft: true, leftValue })
     case MSGS.RIGHT_INPUT:
       if (msg.rightValue === '') {
         return { ...model, sourceLeft: false, rightValue: '', leftValue: '' }
       }
       const rightValue = toInt(msg.rightValue)
-      return { ...model, sourceLeft: false, rightValue }
+      return convert({ ...model, sourceLeft: false, rightValue })
     case MSGS.LEFT_UNIT:
       const { leftUnit } = msg
-      return { ...model, leftUnit }
+      return convert({ ...model, leftUnit })
     case MSGS.RIGHT_UNIT:
       const { rightUnit } = msg
-      return { ...model, rightUnit }
+      return convert({ ...model, rightUnit })
     default:
       return model
   }
